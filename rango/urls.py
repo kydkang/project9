60c5316240a7d41fcb93ac5ea2a4163bfee4c536
lyw112sis <lyw112sis@gmail.com>
2018-04-25 14:27:36 +0700

    After Chapter 06

--- rango/urls.py
+++ rango/urls.py
@@ -1,10 +1,10 @@
-
-
 from django.urls import path
-# from django.conf.urls import url
+from django.conf.urls import url
 from rango import views 
 
 urlpatterns = [
     path('', views.index, name='index'), 
     path('about/', views.about, name='about'), 
+#url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category,name='show_category'),
+    path('category/<slug:category_name_slug>/',views.show_category,name='show_category'), 
 ]
