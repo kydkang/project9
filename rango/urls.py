028c8f73f7faf82c4878965cb8a9e3e93005e564
lyw112sis <lyw112sis@gmail.com>
2018-04-26 10:11:11 +0700

    After Chapter7

--- rango/urls.py
+++ rango/urls.py
@@ -5,6 +5,9 @@ from rango import views
 urlpatterns = [
     path('', views.index, name='index'), 
     path('about/', views.about, name='about'), 
-#url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category,name='show_category'),
+    path('add_category/', views.add_category, name='add_category'), 
+    #url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category,name='show_category'),
     path('category/<slug:category_name_slug>/',views.show_category,name='show_category'), 
+    #url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+    path('category/<slug:category_name_slug>/add_page/',views.add_page, name='add_page'), 
 ]
